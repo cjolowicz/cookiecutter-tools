99088eb7293a2626096e0718c322a0d815bf580b
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-07-17 16:48:57 +0200

    Add extra_context command-line argument

--- src/cookiecutter_tools/create/__main__.py
+++ src/cookiecutter_tools/create/__main__.py
@@ -1,15 +1,19 @@
 """Command-line interface."""
+from typing import Tuple
+
 import click
+from cookiecutter.cli import validate_extra_context
 
 from .create import create
 
 
 @click.command()
 @click.argument("template")
+@click.argument("extra_context", nargs=-1, callback=validate_extra_context)
 @click.version_option()
-def main(template: str) -> None:
+def main(template: str, extra_context: Tuple[str]) -> None:
     """Create a project from a Cookiecutter template."""
-    create(template)
+    create(template, extra_context)
 
 
 if __name__ == "__main__":
