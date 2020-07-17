e67b273e5349a1005783c269b31aadcbd256ca8b
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-07-17 19:10:14 +0200

    Fix missing ellipsis in Tuple annotation

--- src/cookiecutter_tools/create/__main__.py
+++ src/cookiecutter_tools/create/__main__.py
@@ -9,7 +9,7 @@ from cookiecutter import cli
 from .create import create
 
 
-def validate_extra_context(*args: Any) -> Tuple[str]:
+def validate_extra_context(*args: Any) -> Tuple[str, ...]:
     """Validate extra_context command-line argument.
 
     This is a simple wrapper used to simplify the return type.
@@ -28,7 +28,7 @@ def validate_extra_context(*args: Any) -> Tuple[str]:
 @click.argument("template")
 @click.argument("extra_context", nargs=-1, callback=validate_extra_context)
 @click.version_option()
-def main(template: str, extra_context: Tuple[str]) -> None:
+def main(template: str, extra_context: Tuple[str, ...]) -> None:
     """Create a project from a Cookiecutter template."""
     create(template, extra_context)
 
