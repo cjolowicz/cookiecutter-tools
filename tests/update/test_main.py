647f0ada2ca02be012f2426789bdb77fe1208008
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-07-22 17:47:34 +0200

    Add test for cookiecutter-update

--- tests/update/test_main.py
+++ tests/update/test_main.py
@@ -1,10 +1,46 @@
 """Test cases for the __main__ module."""
+import os
+from pathlib import Path
+
 from click.testing import CliRunner
 
-from cookiecutter_tools.update import __main__
+from cookiecutter_tools import git
+from cookiecutter_tools.create import __main__ as create
+from cookiecutter_tools.update import __main__ as update
 
 
 def test_help_succeeds(runner: CliRunner) -> None:
     """It exits with a status code of zero."""
-    result = runner.invoke(__main__.main, ["--help"])
+    result = runner.invoke(update.main, ["--help"])
+    assert result.exit_code == 0
+
+
+def test_update(
+    runner: CliRunner,
+    user_cache_dir: Path,
+    user_config_file: Path,
+    template: git.Repository,
+) -> None:
+    """It generates a project from the template."""
+    runner.invoke(
+        create.main,
+        [str(template.path), f"--config-file={user_config_file}"],
+        input="example",
+        catch_exceptions=False,
+    )
+
+    instance = git.Repository.init(Path("example"))
+    instance.git("add", "--all")
+    instance.git("commit", "--message=Initial")
+    instance.git("branch", "template")
+
+    os.chdir(instance.path)
+
+    (template.path / "{{cookiecutter.project}}" / "LICENSE").touch()
+    template.git("add", ".")
+    template.git("commit", "--message=Add LICENSE")
+    template.git("tag", "v1.1.0")
+
+    result = runner.invoke(update.main)
+    print(result.output)
     assert result.exit_code == 0
