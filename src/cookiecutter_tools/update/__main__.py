82af3f4afcba19f3467b24ffa9acf1fa9e7eebeb
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-07-22 19:07:57 +0200

    Add option --interactive

--- src/cookiecutter_tools/update/__main__.py
+++ src/cookiecutter_tools/update/__main__.py
@@ -8,10 +8,17 @@ from .core import update
 
 @click.command(context_settings={"help_option_names": ["-h", "--help"]})
 @click.argument("extra_context", nargs=-1, callback=validate_extra_context)
+@click.option(
+    "--interactive",
+    is_flag=True,
+    default=False,
+    help="Prompt for parameters",
+    show_default=True,
+)
 @click.version_option(None, "-V", "--version")
-def main(extra_context: StrMapping) -> None:
+def main(extra_context: StrMapping, interactive: bool) -> None:
     """Update a project from a Cookiecutter template."""
-    update(extra_context)
+    update(extra_context, interactive=interactive)
 
 
 if __name__ == "__main__":
