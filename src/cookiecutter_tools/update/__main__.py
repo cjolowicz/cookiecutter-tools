ebdb8be8f2bea2ee3c6eb6733321eba862ef5fb2
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-07-22 18:48:18 +0200

    Add extra_context argument

--- src/cookiecutter_tools/update/__main__.py
+++ src/cookiecutter_tools/update/__main__.py
@@ -1,14 +1,17 @@
 """Command-line interface."""
 import click
 
+from ..create.__main__ import validate_extra_context
+from ..types import StrMapping
 from .core import update
 
 
 @click.command(context_settings={"help_option_names": ["-h", "--help"]})
+@click.argument("extra_context", nargs=-1, callback=validate_extra_context)
 @click.version_option(None, "-V", "--version")
-def main() -> None:
+def main(extra_context: StrMapping) -> None:
     """Update a project from a Cookiecutter template."""
-    update()
+    update(extra_context)
 
 
 if __name__ == "__main__":
